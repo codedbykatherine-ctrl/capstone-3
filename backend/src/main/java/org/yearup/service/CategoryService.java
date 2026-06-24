0b5ddf6e4d67bed830702859f83734ea041d0646
katherine lopez <codedbykatherine@gmail.com>
2026-06-23 21:03:36 -0400

    implemented category with generated id handling

--- backend/src/main/java/org/yearup/service/CategoryService.java
+++ backend/src/main/java/org/yearup/service/CategoryService.java
@@ -33,8 +33,8 @@ public class CategoryService
     public Category create(Category category)
     {
         // create a new category
-        category.setCategoryId(0);
-        return categoryRepository.save(category);
+       category.setCategoryId(0);
+       return categoryRepository.save(category);
     }
 
     public Category update(int id, Category category)
