58f26b8276d9f5115520b1b8b2d72ee90b1a687b
katherine lopez <codedbykatherine@gmail.com>
2026-06-25 12:23:42 -0400

    Add dependency injection for shopping cart and user services

--- backend/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ backend/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,12 @@
 package org.yearup.controllers;
 
+import org.aspectj.weaver.patterns.ThisOrTargetPointcut;
+import org.hibernate.sql.ast.tree.cte.SelfRenderingCteObject;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -9,15 +16,25 @@ import java.security.Principal;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
+@RestController
+@RequestMapping("/cart")
+@PreAuthorize("hasRole('ROLE_USER')")
+@CrossOrigin
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
-    private ShoppingCartService shoppingCartService;
-    private UserService userService;
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
 
+    public ShoppingCartController (ShoppingCartService shoppingCartService, UserService userService){
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+
+    }
 
 
     // each method in this controller requires a Principal object as a parameter
+@GetMapping
     public ShoppingCart getCart(Principal principal)
     {
         // get the currently logged in username
