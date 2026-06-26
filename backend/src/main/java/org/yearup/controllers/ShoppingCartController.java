ff4be4a7618b1d90d09f5560332a508fada5aa22
katherine lopez <codedbykatherine@gmail.com>
2026-06-26 06:09:56 -0400

    Implemented delete all products from the shopping cart endpoint

--- backend/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ backend/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,4 @@
 package org.yearup.controllers;
-
 import org.aspectj.weaver.patterns.ThisOrTargetPointcut;
 import org.hibernate.sql.ast.tree.cte.SelfRenderingCteObject;
 import org.springframework.http.HttpStatus;
@@ -7,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
 import org.yearup.service.UserService;
@@ -20,13 +20,12 @@ import java.security.Principal;
 @RequestMapping("/cart")
 @PreAuthorize("hasRole('ROLE_USER')")
 @CrossOrigin
-public class ShoppingCartController
-{
+public class ShoppingCartController {
     // a shopping cart controller depends on the service la
     private final ShoppingCartService shoppingCartService;
     private final UserService userService;
 
-    public ShoppingCartController (ShoppingCartService shoppingCartService, UserService userService){
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService) {
         this.shoppingCartService = shoppingCartService;
         this.userService = userService;
 
@@ -34,9 +33,8 @@ public class ShoppingCartController
 
 
     // each method in this controller requires a Principal object as a parameter
-@GetMapping
-    public ShoppingCart getCart(Principal principal)
-    {
+    @GetMapping
+    public ShoppingCart getCart(Principal principal) {
         // get the currently logged in username
         String userName = principal.getName();
         // find database user by username
@@ -47,29 +45,48 @@ public class ShoppingCartController
         return shoppingCartService.getByUserId(userId);
     }
 
-    // add a POST method to add a product to the cart - the url should be
-    // https://localhost:8080/cart/products/15  (15 is the productId to be added)
-    // return the updated cart with status 201 Created
+    // add a product to the logged in user's shopping cart
     @PostMapping("/products/{productId}")
-    public ResponseEntity<ShoppingCart> addProductToCart(@PathVariable int productId, Principal principal){
+    //Method returns ResponseEntity containing a shoppingCart
+    // Response Entity lets ypu control the status code and the response body
+    public ResponseEntity<ShoppingCart> addProductToCart(@PathVariable int productId, Principal principal) {
+        // get the username of the currently logged in user
         String username = principal.getName();
+        // use the username to find the user's database id
         int userId = userService.getIdByUsername(username);
-
-        ShoppingCart updatedCart= shoppingCartService.addProduct(userId,productId);
+        // add the selected product to the user's cart and return the updated cart
+        ShoppingCart updatedCart = shoppingCartService.addProduct(userId, productId);
+        // return 201 created with the updated shopping cart
         return ResponseEntity.status(HttpStatus.CREATED).body(updatedCart);
-
-
-
-
     }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated; return the cart (200 OK)
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<ShoppingCart> updateProduct(@PathVariable int productId, @RequestBody ShoppingCartItem shoppingCartItem, Principal principal) {
+        String username = principal.getName();
+        int userId = userService.getIdByUsername(username);
+
+        ShoppingCart shoppingCart = shoppingCartService.updateShoppingCart(userId, productId, shoppingCartItem);
+        if (shoppingCart == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(shoppingCart);
+    }
 
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart  - return the (now empty) cart so the front end can refresh it (200 OK)
+@DeleteMapping
+// maps this method to HTTP delete request and expects an id in the URL
+  public ResponseEntity<ShoppingCart> deleteProducts (Principal principal) {
+ String username = principal.getName();
+ User user = userService.getByUserName(username);
+ shoppingCartService.deleteProducts(user.getId());
+  ShoppingCart cart = shoppingCartService.getByUserId(user.getId());
 
+  return ResponseEntity.status(HttpStatus.OK).body(cart);
+    }
 }
